da746140e37bcb928819aafc4b4e98dd36d56385
Darmaabazar <bazaracer4@gmail.com>
2024-05-12 16:31:37 +0800

    docs folder create

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,20 @@
+require("@babel/polyfill");
 import "../css/style.css";
+import axios from "axios";
+
+
+async function doSearch(search) {
+    try{
+
+        let response = await axios.get(`https://forkify-api.herokuapp.com/api/search?q=${search}`);
+        let recipes = response.data.recipes;
+        console.log(recipes);
+    
+        response = await axios.get(`https://forkify-api.herokuapp.com/api/get?rId=${recipes[1].recipe_id}`);
+        console.log(response.data.recipe.ingredients);
+    } catch(error){
+        alert(error);
+    }
+}
+
+doSearch("pizza");
